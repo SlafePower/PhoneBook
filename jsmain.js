4affcecbc70cf2e18abd9704b9aac616aa910447
SlafePower <anewsoulskeeper@gmail.com>
2018-06-24 09:59:39 +0300

    Add files via upload
    Commit 2.
    Добавлены функции:
     - Создание нового контакта
     - Редактирование контакта
    Требует реализации:
     - Изменение стиля страницы
     - Запись информации о контактах в файл .json
     - Загрузка информации о контактах при первом запуске веб-приложения

--- jsmain.js
+++ jsmain.js
@@ -1,5 +1,41 @@
 var app = angular.module('app', []);
 
-app.controller('testCtrl', function($scope) {
-    $scope.hello = "helloFromJS";
-});
+app.controller
+// angular.module('ng').filter('tel', function (){});
+app.controller('phonebookCtrl', function($scope){
+    $scope.currentPersonId = -1;
+    $scope.phonebook = [];
+    $scope.addNewPerson = function() {
+      if ( $scope.name != '' ) {
+        $scope.phonebook.push({
+          name: $scope.name,
+          phone: $scope.phone,
+          email: $scope.email
+        });
+        $scope.name = '';
+        $scope.phone = '';
+        $scope.email = '';
+      }
+    }
+    $scope.savePerson = function() {
+      if( $scope.currentPersonId > -1 ){
+        var id = $scope.currentPersonId;
+        $scope.phonebook[id].name = $scope.name;
+        $scope.phonebook[id].phone = $scope.phone;
+        $scope.phonebook[id].email = $scope.email;
+        $scope.name = '';
+        $scope.phone = '';
+        $scope.email = '';
+        $scope.currentPersonId = -1;
+      }
+    }
+    $scope.editPerson = function ( id ) {
+      $scope.currentPersonId = id;
+      $scope.name = $scope.phonebook[id].name;
+      $scope.phone = $scope.phonebook[id].phone;
+      $scope.email = $scope.phonebook[id].email;
+    }
+    $scope.deletePerson = function( id ) {
+      $scope.phonebook.splice( id, 1 );
+    }
+  })
